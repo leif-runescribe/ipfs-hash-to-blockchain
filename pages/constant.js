af995a321533c83d39da9b3dda55f903efc68906
Anantabh Sahu <118000382+Anantabh5@users.noreply.github.com>
2024-01-15 17:43:07 +0530

    Update constant.js

--- pages/constant.js
+++ pages/constant.js
@@ -1,5 +1,5 @@
 export const API_URL = " https://rpc-mumbai.maticvigil.com/";
-export const PRIVATE_KEY ="bc171a4b686f6b1564a1e5dea6f0a367f7c7b51e28ed44c6846174824d4ba61e";
+export const PRIVATE_KEY ="bc171a4b686f6b1564a1e5dea6f0a367f7c7b51e28ed44c6846174824d4ba***";
 export const contractAddress = "0xd45F02B649911d4CCbCf86995fD4A096520d6f11";
 export const contractAbi = [
 	{
